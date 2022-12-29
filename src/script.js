321ec8cab360d1df6998365813b889fbf116ac0b
rodrigo antunes <rodrigoka@live.com>
2022-12-29 12:51:20 +0000

    textures test

--- src/script.js
+++ src/script.js
@@ -3,6 +3,27 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 /**
+ * load images (texture)
+ */
+const loadingManager = new THREE.LoadingManager()
+  //i can use for loaded, error, progress...
+loadingManager.onStart = ()=>{
+    console.log('started')
+}
+
+const textureLoader = new THREE.TextureLoader(loadingManager);
+const colorTexture = textureLoader.load('/textures/minecraft.png');
+// const colorTexture = textureLoader.load('/textures/door/color.jpg');
+const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const heightTexture = textureLoader.load('/textures/door/height.jpg')
+const normalTexture = textureLoader.load('/textures/door/normal.jpg')
+const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+
+colorTexture.generateMipmaps = false
+colorTexture.magFilter = THREE.NearestFilter 
+/**
  * Base
  */
 // Canvas
@@ -14,10 +35,35 @@ const scene = new THREE.Scene()
 /**
  * Object
  */
+
+const objPosit = [
+    -1,0,1,
+    0,1,0,
+    0,0,0
+]
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ map: colorTexture })
 const mesh = new THREE.Mesh(geometry, material)
+mesh.position.x = objPosit[0]
 scene.add(mesh)
+
+const geometry2 = new THREE.BoxGeometry(1, 1, 1)
+const material2 = new THREE.MeshBasicMaterial({ map: colorTexture })
+const mesh2 = new THREE.Mesh(geometry2, material2)
+mesh2.position.x = objPosit[1]
+scene.add(mesh2)
+
+const geometry3 = new THREE.BoxGeometry(1, 1, 1)
+const material3 = new THREE.MeshBasicMaterial({ map: colorTexture })
+const mesh3 = new THREE.Mesh(geometry3, material3)
+mesh3.position.x = objPosit[2]
+scene.add(mesh3)
+
+const geometry4 = new THREE.BoxGeometry(1, 1, 1)
+const material4 = new THREE.MeshBasicMaterial({ map: colorTexture })
+const mesh4 = new THREE.Mesh(geometry4, material4)
+mesh4.position.y = objPosit[4]
+scene.add(mesh4)
 
 /**
  * Sizes
