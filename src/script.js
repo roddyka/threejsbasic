8a54b0ac97ea981cee6ce301fd6dab69764f1be6
rodrigo antunes <rodrigoka@live.com>
2023-01-13 18:45:38 +0000

    testure test

--- src/script.js
+++ src/script.js
@@ -20,10 +20,39 @@ const scene = new THREE.Scene()
 /**
  * Object
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const geometry = new THREE.PlaneGeometry(2, 2, 2)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+
+
+// instantiate a loader
+const loader = new THREE.TextureLoader();
+
+// load a resource
+loader.load(
+	// resource URL
+	'textures/TilingStone.png',
+	// onLoad callback
+	function ( texture ) {
+        console.log(texture)
+		// in this example we create the material when the texture is loaded
+		const material = new THREE.MeshBasicMaterial( {
+			map: texture
+		 } );
+         const mesh = new THREE.Mesh(geometry, material)
+         scene.add(mesh)
+	},
+
+	// onProgress callback currently not supported
+	undefined,
+
+	// onError callback
+	function ( err ) {
+		console.error( 'An error happened.' );
+	}
+);
+
+
+
 
 /**
  * Sizes
